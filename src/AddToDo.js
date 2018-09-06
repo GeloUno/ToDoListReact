1cca97c6ebad8cc9947807ccd6e48cbfc69e6217
GeloUno <geloke59@gmail.com>
2018-09-06 14:03:15 +0200

    add file AddToDo.js -> add to thic class function handleChange witch input is changed and update state, handleSubmit stop refresh page after sumit-button press and next state is delete.App.js add render Class Component AddToDo

---
+++ src/AddToDo.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class AddToDo extends Component {
+  state = {
+    title: "",
+    content: ""
+  };
+  handleChange = (e) => {
+  //  console.log(e.target.id);
+    e.target.id ==='title' ? (
+        this.setState({
+            title:e.target.value
+        })
+         // console.log('TITLE')
+    ):(
+        //   console.log('Content')
+        this.setState({
+            content: e.target.value
+        })
+        
+    )};
+    handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(this.state);
+        console.log(e.target);
+        e.target.value = '';
+        this.setState({
+            title:'',
+            content:''
+        })
+        
+    }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="input-field col s12">
+           
+              <input id="title" type="text" onChange={this.handleChange} value={this.state.title} className="validate" />
+              <label className="active"  htmlFor="title"           
+              >
+                Title:
+              </label>
+            </div>
+            <div className="input-field col s12">
+              <input id="content" type="text" onChange={this.handleChange} value={this.state.content} className="validate" />
+              <label className="active" htmlFor="content">
+                Content:
+              </label>
+              </div>
+              <button className="waves-effect purple waves-teal btn" >Send</button>
+         
+        </form>
+      </div>
+    );
+  }
+}
+export default AddToDo;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./ToDoList";
 import ToDoList from "./ToDoList";
+import AddToDo from "./AddToDo";
 
 class App extends Component {
   state = {
@@ -12,20 +13,21 @@ class App extends Component {
     ]
   };
   deleteToDo = (id)=>{
-    console.log(id);
+    const todo1 = this.state.todo.filter((todo)=>{
+      return todo.id !== id; 
+      // if todoid === id; will return one true and other is false; and will leave only true object list, one object list
+      // if todoid!== id will return true for other oject list not equal id sended, so will removed object list witch sended id, and other will stay 
+    });
+    this.setState({todo:todo1})
+    }
 
-  }
+  
   render() {
     return (
       <div className="todoClassCss container">
       <h1 className="center  purple-text">To Do List</h1>
-        <div className="row">
-          <div className="input-field col s12">
-            <input id="title" type="text" className="validate" />
-            <label className="active" htmlFor="title">
-              Title:
-            </label>
-          </div>
+        <div className="row"> 
+        <AddToDo />         
           <ToDoList todo={this.state.todo} deleteToDo={this.deleteToDo} />
         </div>
       </div>
